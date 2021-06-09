1fcca880ebf95469f6ffdc59ff2a56acbe4a10f7
Your Name <your@example.com>
2021-06-09 09:33:46 +0900

    changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# post_list 함수 생성
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+# request를 넘겨받아 render 메소드를 호출
+# 호춣하여 받은 return > blog/post_list.html 템플릿을 보여줌.
