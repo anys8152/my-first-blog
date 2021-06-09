3099741af2a62ad638eb1d886610396ac484a857
Your Name <your@example.com>
2021-06-09 17:11:05 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+    # <int:pk> : 정수 값을 받고 이를 'pk'라는 변수로 뷰에 전송하는 것
+    # post/'정수값'/ --> post_detail이 '정수값'인 것을 찾아 뷰로 전달함.
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
+
+# pk를 식별하기위한 뷰에 매개변수 pk 추가
+# Post.objects.get(pk=pk)
 
 # post_list 함수 생성
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
 # request를 넘겨받아 render 메소드를 호출
 # 호출하여 받은 return > blog/post_list.html 템플릿을 보여줌.
 
