de81f10f6e3eb187ab16a9f1426c1781c4d94210
Your Name <your@example.com>
2021-06-09 14:01:45 +0900

    Modified tempates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
 
 # post_list 함수 생성
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 # request를 넘겨받아 render 메소드를 호출
-# 호춣하여 받은 return > blog/post_list.html 템플릿을 보여줌.
+# 호출하여 받은 return > blog/post_list.html 템플릿을 보여줌.
+
